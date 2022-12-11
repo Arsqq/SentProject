e49d9257f80559aeedc3375c5c3b53ebdef6d655
Arsqq <arny723@gmail.com>
2022-12-11 22:55:55 +0300

    fourth com

--- eurekaclient1/src/main/java/com/example/eurekaclient1/Controllers/SentimentController.java
+++ eurekaclient1/src/main/java/com/example/eurekaclient1/Controllers/SentimentController.java
@@ -1,18 +1,22 @@
 package com.example.eurekaclient1.Controllers;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.ws.rs.core.Response;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/sentiment")
 @RestController
 public class SentimentController {
 
@@ -31,5 +35,20 @@ public class SentimentController {
         public String getTest(){
         return "test";
         }
+
+    @PostMapping("/uploadFile")
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        InputStreamReader isReader = new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8);
+        //Creating a BufferedReader object
+        BufferedReader reader = new BufferedReader(isReader);
+        StringBuilder sb = new StringBuilder();
+        String str;
+        while ((str = reader.readLine()) != null) {
+            sb.append(str);
+            sb.append(System.getProperty("line.separator"));
+        }
+
+        return new ResponseEntity<>(sb, HttpStatus.OK);
+    }
 }
 
