fc472de088878e2a7a332fd8d7455811fff99367
Arsqq <arny723@gmail.com>
2022-12-11 18:58:59 +0300

    third com

--- eurekaclient1/src/main/java/com/example/eurekaclient1/Controllers/MainController.java
+++ eurekaclient1/src/main/java/com/example/eurekaclient1/Controllers/MainController.java
@@ -1,7 +1,25 @@
 package com.example.eurekaclient1.Controllers;
 
+import com.example.eurekaclient1.Model.User;
+import com.example.eurekaclient1.Repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+@RequestMapping("/api/allUsers")
 public class MainController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/listUsers")
+    public List<User> listUsers(){
+        return userRepository.findAll();
+    }
 }
